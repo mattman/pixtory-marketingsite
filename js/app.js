472521d3b48c42f1c9a1d9e84098a440e7c78af0
Matt Didcoe <matt@didcoe.id.au>
2013-06-02 11:14:39 +0800

    moment

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
- $.backstretch("img/header-1.png");
+$.backstretch("img/header-1.png");
+
+function getContent(){
+	moment = getParameterByName('moment');
+	alert(moment);
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var loadMoment = function(momentID, callback) {
+ var url = "http://pixtory.herokuapp.com/moments/" + momentID + "?callback=?";
+ $.getJSON(url).success(function(data) {
+   callback(data.moment);
+ });
+};
