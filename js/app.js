1b55fdce236edf47df8f923f77a098413bbaf3b6
Matt Didcoe <matt@didcoe.id.au>
2013-06-02 23:51:13 +0800

    so I can't do JavaScript anymore...

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ var loadMoment = function(momentID, callback) {
  });
 };
 
-$(document).ready({
+$(document).ready(function(){
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -34,4 +34,4 @@ $(document).ready({
 
 	  ga('create', 'UA-41403739-1', 'ourpixtory.com');
 	  ga('send', 'pageview');	
-})
+});
